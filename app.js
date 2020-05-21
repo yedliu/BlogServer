38e914a565a1c627f682da9ce0a8554486a67828
liuzhiyuan <664182546@qq.com>
2020-05-21 16:14:10 +0800

    save08

--- app.js
+++ app.js
@@ -15,17 +15,17 @@ app.set('view engine', 'ejs');
 
 
 
-// 跨域请求设置
-// app.all('/*', function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
-//   if (req.method == 'OPTIONS') {
-//     res.status(200).end();
-//   } else {
-//     next();
-//   }
-// })
+//跨域请求设置
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+})
 
 app.use(logger('dev'));
 app.use(express.json());
